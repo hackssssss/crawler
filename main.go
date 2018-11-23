0e2e8f93f3a71aee48e7bfa44634dd5e048b0b24
liyunlong <yunlong_li@qingsongchou.com>
2018-11-23 17:56:33 +0800

    增加将电影标题，图片地址，电影详情页利用gorm存储到本地数据库逻辑

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"log"
 	"net/http"
 	"os"
@@ -14,90 +16,34 @@ const StudyGolangurl = `https://www.qiushibaike.com/hot/`
 const Qiushibaikeurl = `https://www.qiushibaike.com/hot/`
 const xia55Url = `https://www.55xia.com/`
 
-func crawlStudyGolang() error {
-	resp, err := http.Get(StudyGolangurl)
-	if err != nil {
-		log.Println("http get fail. err ", err)
-		return err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		log.Println("resp.StatusCode err, ", resp.StatusCode)
-		if resp.StatusCode == 403 {
-			log.Println("403 forbidden")
-			time.Sleep(time.Millisecond * 10)
-			os.Exit(1)
-		}
-		return err
-	}
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		log.Fatal("NewDocument fail, ", err)
-		return err
-	}
-	doc.Find("div.topic dd.right-info").Each(func(i int, content *goquery.Selection) {
-		title := content.Find("div.title").Find("a").Text()
-		strings.Trim(title, "\n")
-		strings.Trim(title, " ")
-		num := content.Find("div.meta").Find("div.num a").Text()
-		strings.Trim(num, "\n")
-		strings.Trim(num, " ")
-		if len(num) == 0 {
-			num = "0"
-		}
-		fmt.Println(title)
-		fmt.Println(num)
-	})
-	return nil
-}
-
-//func test() {
-//	doc, err := goquery.NewDocument(url)
-//}
-
 func TrimString(str string) string {
 	return strings.TrimSpace(strings.Trim(strings.Trim(str, "\n"), " "))
 }
 
-func crawlQiushibaike() error {
-	resp, err := http.Get(Qiushibaikeurl)
-	if err != nil {
-		log.Println("http get fail. err ", err)
-		return err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		log.Println("resp.StatusCode err, ", resp.StatusCode)
-		if resp.StatusCode == 403 {
-			log.Println("403 forbidden")
-			time.Sleep(time.Millisecond * 10)
-			os.Exit(1)
-		}
-		return err
-	}
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-		log.Fatal("NewDocument fail, ", err)
-		return err
-	}
-	//doc.Find("div.content").Each(func (i int, content *goquery.Selection){
-	//	interest := content.Find("")
-	//	strings.Trim(title, "\n")
-	//	strings.Trim(title, " ")
-	//	num := content.Find("div.meta").Find("div.num a").Text()
-	//	strings.Trim(num, "\n")
-	//	strings.Trim(num, " ")
-	//	if len(num) == 0 {
-	//		num = "0"
-	//	}
-	//	fmt.Println(title)
-	//	fmt.Println(num)
-	//})
-	content := doc.Find("div.content").Text()
-	TrimString(content)
-	fmt.Println(content)
+type Crawl55xia struct {
+	Title      string `sql:"column:title" json:"title,omitempty"`             //
+	ImgHref    string `sql:"column:img_href" json:"img_href,omitempty"`       //
+	DetailPage string `sql:"column:detail_page" json:"detail_page,omitempty"` //
+}
 
-	return nil
+func (Crawl55xia) TableName() string {
+	return "crawl55xia"
+}
+
+func write2DB(title, href, filmDetail string) {
+	if title == "" || href == "" || filmDetail == "" {
+		return
+	}
+	data := Crawl55xia{
+		Title:      title,
+		ImgHref:    href,
+		DetailPage: filmDetail,
+	}
+	err := db.Create(&data).Error
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
 
 func crawl55xia() error {
@@ -121,10 +67,10 @@ func crawl55xia() error {
 		log.Fatal("NewDocument fail, ", err)
 		return err
 	}
-	doc.Find(".row").Eq(0).Find(".index-box").Find(".movie-item").Each(func (i int, content *goquery.Selection){
+	doc.Find(".row").Eq(0).Find(".index-box").Find(".movie-item").Each(func(i int, content *goquery.Selection) {
 		title, _ := content.Find(".movie-item-in .lazy").Attr("title")
-		picHref,_ := content.Find(".movie-item-in .lazy").Attr("data-src")
-		filmDetail,_ := content.Find(".movie-item-in a").Attr("href")
+		picHref, _ := content.Find(".movie-item-in .lazy").Attr("data-src")
+		filmDetail, _ := content.Find(".movie-item-in a").Attr("href")
 		title = TrimString(title)
 		picHref = TrimString(picHref)
 		filmDetail = TrimString(filmDetail)
@@ -136,12 +82,27 @@ func crawl55xia() error {
 		fmt.Println(picHref)
 		fmt.Println(filmDetail)
 		fmt.Println("==================")
+		write2DB(title, picHref, filmDetail)
 	})
 	fmt.Println("Done")
 	return nil
 }
 
+var db *gorm.DB
+
+func init() {
+	var err error
+	db, err = gorm.Open("mysql", "root:1234567890@/crawler?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		fmt.Println("err : ", err)
+		os.Exit(0)
+	} else {
+		fmt.Println("Open success,", db)
+	}
+}
+
 func main() {
+
 	count := 0
 	for {
 		if err := crawl55xia(); err != nil {
